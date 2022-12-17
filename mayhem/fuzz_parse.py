b0efa9be4af7719516e944edb1d75654af9ff5d2
bcapuano <bcapuano@asu.edu>
2022-12-17 11:42:06 -0500

    More coverage

--- mayhem/fuzz_parse.py
+++ mayhem/fuzz_parse.py
@@ -1,14 +1,19 @@
 #! /usr/bin/env python3
 import atheris
 import sys
+import fuzz_helpers
 
-with atheris.instrument_imports(include=['pyvips', 'cffi']):
+with atheris.instrument_imports():
     import pyvips
 
 
 def TestOneInput(data):
     try:
-        image = pyvips.Image.new_from_buffer(data, "")
+        fdp = fuzz_helpers.EnhancedFuzzedDataProvider(data)
+        image = pyvips.Image.new_from_buffer(fdp.ConsumeRandomBytes(), "")
+        arr = fuzz_helpers.build_fuzz_list(fdp, [int])
+        mask = pyvips.Image.new_from_array(arr, scale=8)
+        image = image.conv(mask, precision='integer')
     except pyvips.Error:
         return -1
 
